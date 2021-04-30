787486e160204c876971685c77bac8e2eed9a54b
hayata-yamamoto <hayata.pri.yamamoto@gmail.com>
2021-05-01 01:30:52 +0900

    update slack

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { postMessage } from './slack'
-import dayjs from 'dayjs'
-import 'dayjs/locale/ja'
+import { postMessage } from "./slack";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
 
-const now = dayjs(new Date()).locale('ja').format("YYYYMMDD");
-const t = `${now}223000`
+const now = dayjs(new Date()).locale("ja").format("YYYYMMDD");
+const t = `${now}223000`;
 
-const text: string = `今をときめく全盛期ラジオ『問わず語りの神田伯山』はじまりまぁす。 \nhttps://radiko.jp/share/?sid=TBS&t=${t}`
-postMessage(text)
+const text: string = `今をときめく全盛期ラジオ『問わず語りの神田伯山』はじまりまぁす。 \nhttps://radiko.jp/share/?sid=TBS&t=${t}`;
+postMessage(text);
--- src/slack.ts
+++ src/slack.ts
@@ -2,18 +2,21 @@ import { WebClient, WebAPICallResult } from "@slack/web-api";
 
 const token = process.env.SLACK_TOKEN;
 const channelId = process.env.CHANNEL_ID || "";
-const web = new WebClient(token)
+const web = new WebClient(token);
 
 interface ChatPostMessageResult extends WebAPICallResult {
-  channel: string
-  ts: string
+  channel: string;
+  ts: string;
   message: {
-    text: string
-  }
+    text: string;
+  };
 }
 
 const postMessage = async (text: string) => {
-  const res = (await web.chat.postMessage({ text: text, channel: channelId}) as ChatPostMessageResult)
+  const res = (await web.chat.postMessage({
+    text: text,
+    channel: channelId,
+  })) as ChatPostMessageResult;
   console.log("succeeded to postMessage");
 };
 
