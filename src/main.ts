721c0de22d10e3ddbb8a8c7f7c2f9d0d6f1f7a1e
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2021-05-07 22:54:43 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import dayjs from "dayjs";
 import "dayjs/locale/ja";
 
 const now = dayjs(new Date()).locale("ja").format("YYYYMMDD");
-const t = `${now}223000`;
+const t = `${now}213000`;
 
-const text: string = `今をときめく全盛期ラジオ『問わず語りの神田伯山』はじまりまぁす。 \nhttps://radiko.jp/share/?sid=TBS&t=${t}`;
-postMessage(text);
+// const text: string = `今をときめく全盛期ラジオ『問わず語りの神田伯山』はじまりまぁす。 \nhttps://radiko.jp/share/?sid=TBS&t=${t}`;
+// postMessage(text);
+console.log("post")
