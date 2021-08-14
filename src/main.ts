ab21becd2f7b53233159b7b24175a29dd01e2202
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2021-08-14 14:06:51 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,5 @@ import "dayjs/locale/ja";
 const now = dayjs(new Date()).locale("ja").format("YYYYMMDD");
 const t = `${now}213000`;
 
-// const text: string = `今をときめく全盛期ラジオ『問わず語りの神田伯山』はじまりまぁす。 \nhttps://radiko.jp/share/?sid=TBS&t=${t}`;
-// postMessage(text);
-console.log("post")
+const text: string = `今をときめく全盛期ラジオ『問わず語りの神田伯山』はじまりまぁす。 \nhttps://radiko.jp/share/?sid=TBS&t=${t}`;
+postMessage(text);
--- src/slack.ts
+++ src/slack.ts
@@ -3,7 +3,6 @@ import { WebClient, WebAPICallResult } from "@slack/web-api";
 const token = process.env.SLACK_TOKEN;
 const channelId = process.env.CHANNEL_ID || "";
 const web = new WebClient(token);
-
 interface ChatPostMessageResult extends WebAPICallResult {
   channel: string;
   ts: string;
@@ -13,10 +12,7 @@ interface ChatPostMessageResult extends WebAPICallResult {
 }
 
 const postMessage = async (text: string) => {
-  const res = (await web.chat.postMessage({
-    text: text,
-    channel: channelId,
-  })) as ChatPostMessageResult;
+  await web.chat.postMessage({text: text, channel: channelId});
   console.log("succeeded to postMessage");
 };
 
